e24a96ec964d5b6bccb807c877c49c90700ca398
Nathaniel Irons <nirons@methanesat.org>
2024-08-04 19:10:12 -0700

    Correct handling of multiple classes

--- src/annotate-dim-armor-export.py
+++ src/annotate-dim-armor-export.py
@@ -20,6 +20,7 @@ args = parser.parse_args()
 name_idx  = 0
 id_idx    = 2
 tag_idx   = 3
+class_idx = 7
 notes_idx = 32
 
 @dataclass
@@ -27,11 +28,13 @@ class ArmorItem:
 	row: list
 	name: str = ""
 	armor_id: str = ""
+	d2_class: str = ""
 	notes: str = ""
 	
 	def __post_init__(self):
 		self.name = self.row[name_idx]
 		self.armor_id = self.row[id_idx].strip('"')
+		self.d2_class = self.row[class_idx]
 		self.notes = self.row[notes_idx]
 
 	def ignored(self):
@@ -87,6 +90,7 @@ class ArmorProcessor:
 		self.history[letter_grade].append(f"UPO {upo_count}, {d2_class.title()}, {armor.name} (id:{armor.armor_id})")
 
 	def process_armor(self):
+		ids_to_upo_counts = {}
 		for d2_class in ["warlock", "hunter", "titan"]:
 			report_path = f"{self.json_report_dir}/armor-report-{d2_class}.json"
 			if not os.path.exists(report_path):
@@ -96,51 +100,54 @@ class ArmorProcessor:
 					parsed = json.load(f)
 					# extract a mapping of instance IDs to UPO counts
 					mapping = dict([(str(x['id']), x['unique_pinnacle_outfit_count']) for x in parsed])
+					ids_to_upo_counts = ids_to_upo_counts | mapping
 
-				with open(self.armor_csv_path) as csv_file:
-					csv_reader = csv.reader(csv_file, delimiter=',')				
+		if len(ids_to_upo_counts) > 0:
+			with open(self.armor_csv_path) as csv_file:
+				csv_reader = csv.reader(csv_file, delimiter=',')				
 
-					with open(self.output_path, 'w', newline='', encoding="utf-8") as csv_outfile:
-						csv_writer = csv.writer(csv_outfile, delimiter=",")
+				with open(self.output_path, 'w', newline='', encoding="utf-8") as csv_outfile:
+					csv_writer = csv.writer(csv_outfile, delimiter=",")
 
-						header = True
-						for row in csv_reader:
-							if header:
-								try:
-									assert(row[name_idx] == "Name")
-									assert(row[id_idx] == "Id")
-									assert(row[notes_idx] == "Notes")
-								except AssertionError as e:
-									print("ERROR: looks like DIM changed its CSV columns again. Can't proceed.")
-									raise(e)	
-								csv_writer.writerow(row)
-								header = False
-								continue
+					header = True
+					for row in csv_reader:
+						if header:
+							try:
+								assert(row[name_idx] == "Name")
+								assert(row[id_idx] == "Id")
+								assert(row[class_idx] == "Equippable")
+								assert(row[notes_idx] == "Notes")
+							except AssertionError as e:
+								print("ERROR: looks like DIM changed its CSV columns again. Can't proceed.")
+								raise(e)	
+							csv_writer.writerow(row)
+							header = False
+							continue
 
-							armor = ArmorItem(row)
-							if armor.armor_id in mapping:
-								upo_count = mapping[armor.armor_id]
-								if armor.ignored():
-									pass
-								elif upo_count == 0:
-									self.add_to_history("F", armor, upo_count, d2_class)
-									armor.add_upo_note("#UPO-F-0")
-								elif upo_count < 5:
-									self.add_to_history("D", armor, upo_count, d2_class)
-									armor.add_upo_note(f"#UPO-D-{upo_count}")
-								elif upo_count < 11:
-									self.add_to_history("C", armor, upo_count, d2_class)
-									armor.add_upo_note(f"#UPO-C-{upo_count}")
-								elif upo_count < 31:
-									self.add_to_history("B", armor, upo_count, d2_class)
-									armor.add_upo_note(f"#UPO-B-{upo_count}")
-								elif upo_count < 50:
-									self.add_to_history("A", armor, upo_count, d2_class)
-									armor.add_upo_note(f"#UPO-A-{upo_count}")
-								elif upo_count >= 50:
-									self.add_to_history("S", armor, upo_count, d2_class)
-									armor.add_upo_note(f"#UPO-S-{upo_count}")
-							csv_writer.writerow(armor.row)
+						armor = ArmorItem(row)
+						if armor.armor_id in ids_to_upo_counts:
+							upo_count = ids_to_upo_counts[armor.armor_id]
+							if armor.ignored():
+								pass
+							elif upo_count == 0:
+								self.add_to_history("F", armor, upo_count, armor.d2_class)
+								armor.add_upo_note("#UPO-F-0")
+							elif upo_count < 5:
+								self.add_to_history("D", armor, upo_count, armor.d2_class)
+								armor.add_upo_note(f"#UPO-D-{upo_count}")
+							elif upo_count < 11:
+								self.add_to_history("C", armor, upo_count, armor.d2_class)
+								armor.add_upo_note(f"#UPO-C-{upo_count}")
+							elif upo_count < 31:
+								self.add_to_history("B", armor, upo_count, armor.d2_class)
+								armor.add_upo_note(f"#UPO-B-{upo_count}")
+							elif upo_count < 50:
+								self.add_to_history("A", armor, upo_count, armor.d2_class)
+								armor.add_upo_note(f"#UPO-A-{upo_count}")
+							elif upo_count >= 50:
+								self.add_to_history("S", armor, upo_count, armor.d2_class)
+								armor.add_upo_note(f"#UPO-S-{upo_count}")
+						csv_writer.writerow(armor.row)
 		
 		for letter_grade in ["F", "D", "C", "B", "A", "S"]:
 			if letter_grade in self.history:
